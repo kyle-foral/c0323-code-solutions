6322cdc075e9053811cbdf59a9e565ae8ac24cc7
Kyle Foral <kyleforal@gmail.com>
2023-04-19 17:47:11 +0000

    updated circle.js and square.js

--- es6-classes/circle.js
+++ es6-classes/circle.js
@@ -2,11 +2,15 @@
 /* exported Circle */
 
 class Circle extends Shape {
-  constructor(radius) { super(); }
+  constructor(radius) {
+    super(area, perimeter);
+    this.radius = radius;
+  }
+
   describe() {
     return 'This is the radius ' + this.radius;
   }
 }
 
-const circle = new Circle();
+const circle = new Circle(10);
 console.log('circle radius', circle.describe);
--- es6-classes/square.js
+++ es6-classes/square.js
@@ -2,11 +2,15 @@
 /* exported Square */
 
 class Square extends Shape {
-  constructor(width) { super(); }
+  constructor(width) {
+    super(area, perimeter);
+    this.width = width;
+  }
+
   describe() {
     return 'This is the width ' + this.width;
   }
 }
 
-const square = new Square();
+const square = new Square(4);
 console.log('square width', square.describe);
