16ef09579ed557fac662dd7ef2e4b5f651ea879c
Kyle Foral <kyleforal@gmail.com>
2023-05-11 22:27:43 +0000

    fixed a bit to clean up

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,8 +2,7 @@ import * as fs from 'node:fs/promises';
 
 try {
   const number = Math.random();
-  const text = number.toString();
-  await fs.writeFile('random.txt', `${text}\n`);
+  await fs.writeFile('random.txt', `${number}\n`);
 } catch (err) {
   console.error(err);
 }
