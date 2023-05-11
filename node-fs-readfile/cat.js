b9300acd140eea8b1ace9a8a7b1338e9932418dc
Kyle Foral <kyleforal@gmail.com>
2023-05-11 20:42:23 +0000

    moving onto next assignment, still on cat.js

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,11 @@
+// import { readFile } from 'node:fs/promises'
+// try {
+//   const text = process.argv.slice(2)
+//   const file = text.map((element) => readFile(element), 'utf8')
+
+//   console.log(contents)
+// } catch (err) {
+//   console.error(err.message)
+// }
+
+// //const contents = await readFile(text, { encoding: 'utf8' })
---
+++ node-fs-readfile/read-any.js
@@ -0,0 +1,7 @@
+import { readFile } from 'node:fs/promises';
+try {
+  const contents = await readFile(process.argv[2], { encoding: 'utf8' });
+  console.log(contents);
+} catch (err) {
+  console.error(err.message);
+}
