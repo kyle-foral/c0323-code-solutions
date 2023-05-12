4ae6d4410d49ee159b7f8c2faa840de54fe7362a
Kyle Foral <kyleforal@gmail.com>
2023-05-12 05:28:49 +0000

    finished add

---
+++ node-notes-cli/app.js
@@ -0,0 +1,29 @@
+// import * as fs from 'node:fs/promises'
+
+// try {
+//   if (process.argv[2] === 'read') {
+//     read()
+//   }
+//   if (process.argv[2] === 'create') {
+
+//   }
+//   if (process.argv[2] === 'delete') {
+
+//   }
+//   if (process.argv[2] === 'update') {
+
+//   }
+// } catch (err) {
+//   console.error('Error:', err.message)
+// }
+
+// async function read () {
+//   const content = await fs.readFile('data.json')
+//   const info = JSON.parse(content)
+//   console.log(info)
+// }
+
+// async function create () {
+//   const file = process.argv[4]
+//   const input = process.argv[5]
+// }
