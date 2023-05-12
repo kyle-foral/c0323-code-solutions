b66d6c8dfff22a06c7df03bb0382ca434033b71d
Kyle Foral <kyleforal@gmail.com>
2023-05-12 04:16:20 +0000

    finished node-fs-readfile

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -4,7 +4,7 @@ const [, , ...files] = process.argv;
 const promises = files.map((files) => readFile(files, 'utf8'));
 try {
   const contents = await Promise.all(promises);
-  console.log(`${contents}\n`);
+  console.log(contents.join('\n'));
 } catch (err) {
   console.error(err.message);
 }
