c9f9eef3d2e396130c07485dabcfed6f09a55fae
Kyle Foral <kyleforal@gmail.com>
2023-05-18 17:51:59 +0000

    fixed post/put/delete

--- sgt-back-end/server.js
+++ sgt-back-end/server.js
@@ -40,12 +40,10 @@ app.post('/api/grades', async (req, res) => {
       Number(req.body.score) > 100 ||
       Number(req.body.score) < 0
     ) {
-      res
-        .status(400)
-        .json({
-          error:
-            'name and course is a required field that is valid. Score must be a valid number between 0 and 100',
-        });
+      res.status(400).json({
+        error:
+          'name and course is a required field that is valid. Score must be a valid number between 0 and 100',
+      });
       return;
     }
     const text = `
@@ -65,8 +63,8 @@ app.put('/api/grades/:gradeId', async (req, res) => {
   try {
     console.log(req.method);
     const id = Number(req.params.gradeId);
-    let foundGradeId = false;
-    if (id < 0 || Number.NaN(id)) {
+    const foundGradeId = false;
+    if (id < 0 || Number.isNaN(id)) {
       res.status(400).json({ error: `${id} is an invalid number` });
       return;
     }
@@ -77,44 +75,30 @@ app.put('/api/grades/:gradeId', async (req, res) => {
       Number(req.body.score) > 100 ||
       Number(req.body.score) < 0
     ) {
-      res
-        .status(400)
-        .json({
-          error:
-            'name and course is a required field that is valid. Score must be a valid number between 0 and 100',
-        });
+      res.status(400).json({
+        error:
+          'name and course is a required field that is valid. Score must be a valid number between 0 and 100',
+      });
       return;
     }
-    const selectSql = `SELECT "gradeId",
-                            "name",
-                            "course",
-                            "score",
-                            "createdAt"
-                      FROM "grades"`;
-    db.query(selectSql, (res3) => {
-      for (let i = 0; i < res3.rows.length; i++) {
-        if (res3.rows[i].gradeId === id) {
-          foundGradeId = true;
-          const text = `UPDATE grades
+    const text = `UPDATE grades
                       SET name=$1,
                         course=$2,
                         score=$3
                         where "gradeId"=$4
                         RETURNING*`;
-          const values = [req.body.name, req.body.course, req.body.score, id];
+    const values = [req.body.name, req.body.course, req.body.score, id];
 
-          db.query(text, values, (res2) => {
-            return res.status(200).json(res2.rows[0]);
-          });
-          break;
-        }
-      }
-      if (foundGradeId === false) {
-        return res
-          .status(404)
-          .json({ error: 'the gradeId entered does not exist' });
-      }
-    });
+    const result = await db.query(text, values);
+    const grade = result.rows[0];
+    if (grade) {
+      res.status(200).json(grade);
+    }
+    if (foundGradeId === false) {
+      return res
+        .status(404)
+        .json({ error: 'the gradeId entered does not exist' });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An unexpected error occurred.' });
@@ -125,38 +109,39 @@ app.delete('/api/grades/:gradeId', async (req, res) => {
   try {
     console.log(req.method);
     const id = Number(req.params.gradeId);
-    let foundGradeId = false;
+    const foundGradeId = false;
     if (id < 0 || Number.isNaN(id)) {
       return res
         .status(400)
         .json({ error: 'gradeId must be a positive integer' });
     }
-    const sqlSelect = `SELECT "gradeId",
-                          "name",
-                          "course",
-                          "score",
-                          "createdAt"
-                      FROM "grades"`;
-    db.query(sqlSelect, (res3) => {
-      for (let i = 0; i < res3.rows.length; i++) {
-        if (res3.rows[i].gradeId === id) {
-          foundGradeId = true;
-          const text = `DELETE FROM "grades"
+    if (
+      req.body.name === undefined ||
+      req.body.course === undefined ||
+      req.body.score === undefined ||
+      Number(req.body.score) > 100 ||
+      Number(req.body.score) < 0
+    ) {
+      res.status(400).json({
+        error:
+          'name and course is a required field that is valid. Score must be a valid number between 0 and 100',
+      });
+      return;
+    }
+    const text = `DELETE FROM "grades"
                           where "gradeId"=$1
                           RETURNING*`;
-          const values = [id];
-          db.query(text, values, (res2) => {
-            return res.sendStatus(204);
-          });
-          break;
-        }
-      }
-      if (foundGradeId === false) {
-        return res
-          .status(404)
-          .json({ error: 'the gradeId entered does not exist' });
-      }
-    });
+    const values = [id];
+    const result = await db.query(text, values);
+    const grade = result.rows[0];
+    if (grade) {
+      return res.sendStatus(204);
+    }
+    if (foundGradeId === false) {
+      return res
+        .status(404)
+        .json({ error: 'the gradeId entered does not exist' });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An unexpected error occurred.' });
