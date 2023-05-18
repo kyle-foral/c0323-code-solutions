cc1be7e12f190261bfea62d0a9fd7764b949bc48
Kyle Foral <kyleforal@gmail.com>
2023-05-18 21:10:46 +0000

    fixed imports

--- webpack-and-babel/imports.js
+++ webpack-and-babel/imports.js
@@ -2,7 +2,7 @@
 
 // import from a file located at the absolute path `/foo/bar/baz/qux.js`:
 // eslint-disable-next-line import/no-absolute-path
-import f1 from '/foo/bar/baz/qux.js';
+import f1 from '/foo/bar/baz/qux';
 
 // import from a file named `foo.json` located in the current directory:
 import f2 from '../foo.json';
@@ -12,11 +12,11 @@ import f3 from '../foo/bar.js';
 
 // import from a file named `index.js` located in a directory named `baz` that
 // is in the current directory:
-import f4 from '../../baz/index.js';
+import f4 from '../../baz';
 
 // import from a file named `index.js` located in a directory named `baz` that
 // is in the parent directory:
-import f5 from '../baz/index.js';
+import f5 from '../baz';
 
 // import from an `npm` module named `react`:
 import f6 from 'react';
