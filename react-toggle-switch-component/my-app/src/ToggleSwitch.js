41a3428e2a429d33acb58bee8fedee8fe7bb1917
Kyle Foral <kyleforal@gmail.com>
2023-05-22 21:16:41 +0000

    WIP

---
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -0,0 +1,16 @@
+// import { useState } from "react"
+import './ToggleSwitch.css';
+
+export default function ToggleSwitch() {
+  // const [toggle, setToggle] = useState(false);
+
+  // function handleClick(){
+  //   setToggle(!toggle)
+  // }
+
+  return (
+    <label class="toggle-switch">
+      <input type="checkbox" />
+    </label>
+  );
+}
