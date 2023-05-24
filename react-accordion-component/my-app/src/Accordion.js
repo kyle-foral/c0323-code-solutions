dfc8228eae3e2e374431a22007dd5607aae69d7a
Kyle Foral <kyleforal@gmail.com>
2023-05-24 19:27:11 +0000

    finished react-accordion-component

--- react-accordion-component/my-app/src/Accordion.js
+++ react-accordion-component/my-app/src/Accordion.js
@@ -1,56 +1,39 @@
-// import {useState} from 'react'
+import { useState } from 'react';
+import './Accordion.css';
 
-// const topics = [
-//   {
-//     id: 1,
-//     title: 'HTML',
-//     content:
-//       'Hypertext Markup Language (HTML) is the standard markup Language for creating web pages and web applications. With Cascasing Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.',
-//   },
-//   {
-//     id: 2,
-//     title: 'CSS',
-//     content:
-//       'Cascading Style Sheets (CSS) is a style sheet Language used for describing the presentation of a document written in markup Language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.',
-//   },
-//   {
-//     id: 3,
-//     title: 'JavaScript',
-//     content:
-//       'JavaScript, often abbreviated as JS is a high-level, interpreted programming Language that confroms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation and first-class functions.',
-//   },
-// ];
+export default function Accordion({ topics }) {
+  const [current, setCurrent] = useState(null);
 
-// export default function Accordion([{topics}]){
-// const [activeIndex, setActiveIndex] = useState(0)
-//   return (
-//     <div>
-//       <Panel
-//         title="Hypertext Markup Language"
-//         id={activeIndex === 0}
-//         onClick={() => setActiveIndex(0)}
-//       >
-//       </Panel>
-//       <Panel
-//         title="Cascading Style Sheets"
-//         id={activeIndex === 1}
-//         onClick={() => setActiveIndex(1)}
-//       >
-//       </Panel>
-//       <Panel
-//         title="JavaScript"
-//         id={activeIndex === 2}
-//         onClick={() => setActiveIndex(2)}
-//       >
-//       </Panel>
-//     </div>
-//   );
-// }
+  const lists = [];
+  for (let i = 0; i < topics.length; i++) {
+    lists.push(
+      <Panel
+        key={topics[i].id}
+        title={topics[i].title}
+        isActive={current === i}
+        onShow={handleClick}
+        content={topics[i].content}
+      />
+    );
+    function handleClick() {
+      if (current === topics[i].id) {
+        setCurrent(null);
+      } else {
+        setCurrent(topics[i].id);
+      }
+    }
+  }
+  return <div>{lists}</div>;
+}
 
-// function Panel ({topics}){
-//   return
-//   <section className="panel">
-//     <h3>{title}</h3>
-//     <p> {content} </p>
-//   </section>;
-// }
+function Panel({ title, content, isActive, onShow }) {
+  return (
+    <section className="panel">
+      <button onClick={onShow} className="title">
+        {' '}
+        {title}{' '}
+      </button>
+      {isActive && <p className="text">{content}</p>}
+    </section>
+  );
+}
--- react-accordion-component/my-app/src/App.js
+++ react-accordion-component/my-app/src/App.js
@@ -1,32 +1,33 @@
-// import Accordion from './Accordion';
-// import './App.css';
+import Accordion from './Accordion';
+import './App.css';
 
-// function App() {
-//   const topics = [
-//     {
-//       id: 1,
-//       title: 'HTML',
-//       content:
-//         'Hypertext Markup Language (HTML) is the standard markup Language for creating web pages and web applications. With Cascasing Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.',
-//     },
-//     {
-//       id: 2,
-//       title: 'CSS',
-//       content:
-//         'Cascading Style Sheets (CSS) is a style sheet Language used for describing the presentation of a document written in markup Language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.',
-//     },
-//     {
-//       id: 3,
-//       title: 'JavaScript',
-//       content:
-//         'JavaScript, often abbreviated as JS is a high-level, interpreted programming Language that confroms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation and first-class functions.',
-//     },
-//   ];
-//   return (
-//     <div>
-//     <Accordion/>
-//     </div>
-//   );
-// }
+const topics = [
+  {
+    id: 0,
+    title: 'HTML',
+    content:
+      'Hypertext Markup Language (HTML) is the standard markup Language for creating web pages and web applications. With Cascasing Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.',
+  },
+  {
+    id: 1,
+    title: 'CSS',
+    content:
+      'Cascading Style Sheets (CSS) is a style sheet Language used for describing the presentation of a document written in markup Language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.',
+  },
+  {
+    id: 2,
+    title: 'JavaScript',
+    content:
+      'JavaScript, often abbreviated as JS is a high-level, interpreted programming Language that confroms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation and first-class functions.',
+  },
+];
 
-// export default App;
+function App() {
+  return (
+    <div>
+      <Accordion topics={topics} />
+    </div>
+  );
+}
+
+export default App;
