847e00e19274fdfaa213f4c0000d70825f5cfdf9
Kyle Foral <kyleforal@gmail.com>
2023-05-26 19:02:24 +0000

    fixed error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,10 +1,11 @@
 async function logData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) throw new Error(`fetch Error ${response.status}`);
     const jsonData = await response.json();
     console.log('Worked', jsonData);
   } catch (error) {
-    console.error('error', error);
+    console.error(error);
   }
 }
 logData();
@@ -12,10 +13,11 @@ logData();
 async function pokemonData() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    if (!response.ok) throw new Error(`fetch Error ${response.status}`);
     const jsonData = await response.json();
     console.log('Ditto', jsonData);
   } catch (error) {
-    console.error('error', error);
+    console.error(error);
   }
 }
 pokemonData();
