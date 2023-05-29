874f18417f7e14cbe790f7949f381bb6dfdf232d
Kyle Foral <kyleforal@gmail.com>
2023-05-29 16:56:28 +0000

    WIP fetch user

---
+++ fetch-in-react/fetch-in-react-quiz-notes.md
@@ -0,0 +1,44 @@
+# fetch-in-react-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- How can `useEffect` be used to load data for a component?
+  by using a fetch and adding dependencies array
+- What browser function can be used to make HTTP requests to a server in React?
+  fetch()
+- How do you use `useEffect` to load component data just once when the component mounts?
+  pass the empty array as a second argument to useEffect hook
+- How do you use `useEffect` to load component data every time the data key changes?
+  by adding into the dependencies array
+- In a large-scale production app, what are some better alternatives for loading and managing backend data?
+  manage all your data accesses with a third-party data management library. Vercel SWR and React Query
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ fetch-in-react/my-app/src/User.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import UserCard from './UserCard';
+
+export default function User({ userId, onCancel }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+  const [user, setUser] = useState();
+
+  /* your code here (hint: useEffect) */
+
+  useEffect(() => {
+    async function logData(userId) {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users/${userId}'
+        );
+        if (!response.ok) throw new Error(`fetch Error ${response.status}`);
+        const jsonData = await response.json();
+        if (jsonData) {
+          setIsLoading(false);
+          setUser(jsonData);
+          console.log('Correct', jsonData);
+        }
+      } catch (error) {
+        setIsLoading(false);
+        setError(error);
+        console.error('Error', error);
+      }
+    }
+    if (userId) logData(userId);
+  }, [userId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    console.error('Fetch error:', error);
+    return <p>Error! {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <button onClick={() => onCancel()}>Close</button>
+      <UserCard user={user} />
+    </div>
+  );
+}
