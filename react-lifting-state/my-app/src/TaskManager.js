62017ea853df7053f54d98032723b06c28d05399
Kyle Foral <kyleforal@gmail.com>
2023-06-04 18:12:43 +0000

    finished react-lifting-state

--- react-lifting-state/my-app/src/TaskManager.js
+++ react-lifting-state/my-app/src/TaskManager.js
@@ -1,4 +1,4 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 /**
  * Manages outstanding and completed tasks.
@@ -9,86 +9,64 @@
  *   - tasks, an Array of { id: number, name: string, completed: boolean }
  * TODO: It doesn't work!
  */
-// export default function TaskManager({ tasks }) {
-//   const [completed, setCompleted] = useState({});
-//   const [filter, setFilter] = useState('');
-//   return (
-//     <div>
-//       <Filter />
-//       <div>
-//         <OutstandingTasks
-//         tasks={tasks}
-//         completed={completed} />
-//         <CompletedTasks tasks={[]} />
-//       </div>
-//     </div>
-//   );
-// }
+export default function TaskManager({ tasks }) {
+  const [completed, setCompleted] = useState({});
+  const outstanding = tasks.filter((task) => !completed.includes(task));
 
-/**
- * An input field that can be used to filter a list of items.
- */
-// function Filter({filter}) {
-//   const [filter, setFilter] = useState('');
-//   return (
-//     <div>
-//       <span>Filter: </span>
-//       <input
-//         type="text"
-//         value={filter}
-//         onChange={(e) => setFilter(e.target.value)}
-//       />
-//     </div>
-//   );
-// }
+  function handleComplete(task) {
+    setCompleted([...completed, task]);
+  }
+  return (
+    <div>
+      <OutstandingTasks
+        outstanding={outstanding}
+        onCompleted={handleComplete}
+      />
+      <CompletedTasks completed={completed} />
+    </div>
+  );
+}
 
 /**
  * An array of tasks with a checkbox to indicate they are completed.
  * Props:
  *   - tasks, the tasks to display
  */
-// function OutstandingTasks({ tasks, completed }) {
-//   // Tracks completed state of all tasks. key = task.id, value = task.completed
-//   // const [completed, setCompleted] = useState({});
-//   if (tasks.length === 0) return <div>No outstanding tasks.</div>;
-//   return (
-//     <div>
-//       <div>Outstanding Tasks</div>
-//       <ul style={{ listStyleType: 'none' }}>
-//         {tasks.map((task) => (
-//           <li key={task.id}>
-//             <label>
-//               <input
-//                 type="checkbox"
-//                 checked={!!completed[task.id]}
-//                 onChange={() =>
-//                 setCompleted({ ...completed, [task.id]: !task.completed })
-//                 }
-//               />
-//               <span>{task.name}</span>
-//             </label>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+function OutstandingTasks({ outstanding, onComplete }) {
+  // Tracks completed state of all tasks. key = task.id, value = task.completed
+  if (outstanding.length === 0) return <div>No outstanding tasks.</div>;
+  return (
+    <div>
+      <div>Outstanding Tasks</div>
+      <ul style={{ listStyleType: 'none' }}>
+        {outstanding.map((task) => (
+          <li key={task.id}>
+            <label>
+              <input type="checkbox" onChange={() => onComplete(task)} />
+              <span>{task.name}</span>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 /**
  * An array of tasks.
  * Props:
  *   - tasks, the tasks to display
  */
-// function CompletedTasks({ tasks }) {
-//   if (tasks.length === 0) return <div>No completed tasks.</div>;
-//   return (
-//     <div>
-//       <div>Completed Tasks</div>
-//       <ul style={{ listStyleType: 'none' }}>
-//         {tasks.map((task) => (
-//         <li key={task.id}>{task.name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+function CompletedTasks({ completed }) {
+  if (completed.length === 0) return <div>No completed tasks.</div>;
+  return (
+    <div>
+      <div>Completed Tasks</div>
+      <ul style={{ listStyleType: 'none' }}>
+        {completed.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
