4853f3fe66db4898e1c177ad0c1efd5fff48f9f4
Kyle Foral <kyleforal@gmail.com>
2023-06-26 21:36:41 +0000

    finished object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,9 @@
 /* exported invert */
+
+function invert(source) {
+  const newObject = {};
+  for (const [key, value] of Object.entries(source)) {
+    newObject[value] = key;
+  }
+  return newObject;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,3 +1,11 @@
 /* exported pick */
 
-function pick(source, keys) {}
+function pick(source, keys) {
+  const newObject = {};
+  for (const key of keys) {
+    if (source[key] !== undefined) {
+      newObject[key] = source[key];
+    }
+  }
+  return newObject;
+}
